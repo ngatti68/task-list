d09ce6f113a9d85446d76a7e65d8f9b160cfbefa
ngatti68 <norberto.o.gatti@gmail.com>
2022-11-24 22:31:46 -0300

    Agregamos componentes taks y taks-item

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,32 @@
+import { Task } from "./Task"
+
+export const TASK: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer módulo de Angular",
+        day: 'Diciembre 5 20.00 horas',
+        reminder: true,
+    },
+
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: 'Diciembre 6 18.00 horas',
+        reminder: true,
+    },
+
+    {
+        id: 1,
+        text: "Investigar sobre Boostrap",
+        day: 'Diciembre 7 10.00 horas',
+        reminder: false,
+    },
+
+    {
+        id: 1,
+        text: "Leer mi libro favorito",
+        day: 'Diciembre 12 14.00 horas',
+        reminder: false,
+    },
+
+]
